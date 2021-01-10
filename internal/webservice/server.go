763a130f6b04ff79744a0fc85c9aa11d2c7a0298
Egor Lynko <flexoid@gmail.com>
2021-01-10 22:10:41 +0300

    Implement proper logging

--- internal/webservice/server.go
+++ internal/webservice/server.go
@@ -1,13 +1,14 @@
 package webservice
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
 
 	"github.com/Logiraptor/go-pivotaltracker/v5/pivotal"
+	"github.com/rs/zerolog/log"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
 
@@ -25,7 +26,7 @@ func (s *Server) Start() {
 	http.HandleFunc("/events-endpoint", s.eventsHandler)
 	http.HandleFunc("/interactive-endpoint", s.interactiveHandler)
 
-	fmt.Println("[INFO] Server listening")
+	log.Info().Msgf("Listening on port %s", s.Port)
 	http.ListenAndServe(":"+s.Port, nil)
 }
 
@@ -34,29 +35,39 @@ func (s *Server) eventsHandler(w http.ResponseWriter, r *http.Request) {
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
+		log.Warn().Err(err).Msg("Could not read event body")
 		w.WriteHeader(http.StatusBadRequest)
+
 		return
 	}
 
 	sv, err := slack.NewSecretsVerifier(r.Header, s.SlackSigningSecret)
 	if err != nil {
+		log.Warn().Err(err).Msg("Could not initialize slack secrets verifier")
 		w.WriteHeader(http.StatusBadRequest)
+
 		return
 	}
 
 	if _, err = sv.Write(body); err != nil {
+		log.Warn().Err(err).Msg("Secrets verifier write failed")
 		w.WriteHeader(http.StatusInternalServerError)
+
 		return
 	}
 
 	if err = sv.Ensure(); err != nil {
+		log.Warn().Err(err).Msg("Slack signature validation failed")
 		w.WriteHeader(http.StatusUnauthorized)
+
 		return
 	}
 
 	eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
 	if err != nil {
+		log.Warn().Err(err).Msg("Could not parse slack event")
 		w.WriteHeader(http.StatusInternalServerError)
+
 		return
 	}
 
@@ -68,12 +79,16 @@ func (s *Server) interactiveHandler(w http.ResponseWriter, r *http.Request) {
 	err := json.Unmarshal([]byte(r.FormValue("payload")), &payload)
 
 	if err != nil {
-		fmt.Printf("Could not parse action response JSON: %v", err)
+		log.Warn().Err(err).Msgf("Could not parse inveractive action payload")
+		return
 	}
+
+	logger := log.With().Str("trigger_id", payload.TriggerID).Logger()
+	ctx := logger.WithContext(context.TODO())
 
 	for _, blockAction := range payload.ActionCallback.BlockActions {
 		if blockAction.ActionID == messages.ActionShowMore {
-			s.handleExpandAction(payload, blockAction)
+			go s.handleExpandAction(ctx, &payload, blockAction)
 		}
 	}
 }
@@ -84,7 +99,9 @@ func (s *Server) handleSlackEvent(event *slackevents.EventsAPIEvent, body []byte
 		err := json.Unmarshal(body, &r)
 
 		if err != nil {
+			log.Warn().Err(err).Msg("Could not unmarshal slack challenge response body")
 			w.WriteHeader(http.StatusInternalServerError)
+
 			return
 		}
 
@@ -93,27 +110,42 @@ func (s *Server) handleSlackEvent(event *slackevents.EventsAPIEvent, body []byte
 	}
 
 	if event.Type == slackevents.CallbackEvent {
+		var callbackEvent slackevents.EventsAPICallbackEvent
+		err := json.Unmarshal(body, &callbackEvent)
+
+		if err != nil {
+			log.Warn().Err(err).Msg("Could not unmarshal slack callback event")
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		logger := log.With().Str("event_id", callbackEvent.EventID).Logger()
+		ctx := logger.WithContext(context.TODO())
+
 		innerEvent := event.InnerEvent
 		switch ev := innerEvent.Data.(type) {
 		case *slackevents.MessageEvent:
-			go s.handleMessage(ev)
+			go s.handleMessage(ctx, ev)
 		}
 	}
 }
 
-func (s *Server) handleMessage(event *slackevents.MessageEvent) {
+func (s *Server) handleMessage(ctx context.Context, event *slackevents.MessageEvent) {
 	ids := messages.ExtractStoriesFromMessage(event.Text)
 
 	if len(ids) == 0 {
 		return
 	}
 
+	log.Ctx(ctx).Info().Ints("stories", ids).Msg("Received message with pivotal stories mentioned")
+
 	var stories []*pivotal.Story
 
 	for _, id := range ids {
 		story, _, err := s.PivotalClient.Stories.GetByID(id)
 		if err != nil {
-			fmt.Printf("%s", err)
+			log.Ctx(ctx).Warn().Err(err).Msgf("Cannot fetch pivotal story %d", id)
 			continue
 		}
 
@@ -121,7 +153,7 @@ func (s *Server) handleMessage(event *slackevents.MessageEvent) {
 	}
 
 	if len(stories) == 0 {
-		fmt.Printf("No stories are fetched successfully\n")
+		log.Ctx(ctx).Debug().Msg("No mentioned stories were fetched successfully, nothing to post")
 		return
 	}
 
@@ -135,20 +167,22 @@ func (s *Server) handleMessage(event *slackevents.MessageEvent) {
 
 	_, _, err := s.SlackClient.PostMessage(event.Channel, options...)
 	if err != nil {
-		fmt.Printf("%s", err)
+		log.Ctx(ctx).Warn().Err(err).Msgf("Cannot post slack message in response to mentioned stories")
 		return
 	}
+
+	log.Ctx(ctx).Info().Msg("Slack message with mentioned stories is posted")
 }
 
-func (s *Server) handleExpandAction(payload slack.InteractionCallback, blockAction *slack.BlockAction) {
+func (s *Server) handleExpandAction(ctx context.Context, payload *slack.InteractionCallback, blockAction *slack.BlockAction) {
 	pivotalStoryID, err := strconv.Atoi(blockAction.Value)
 	if err != nil {
-		fmt.Printf("Unexpected value for expand block action: %v", blockAction.Value)
+		log.Ctx(ctx).Warn().Err(err).Msgf("Unexpected value for expand block action: %v", blockAction.Value)
 	}
 
 	story, _, err := s.PivotalClient.Stories.GetByID(pivotalStoryID)
 	if err != nil {
-		fmt.Printf("Failed to fetch story: %s", err)
+		log.Ctx(ctx).Warn().Err(err).Msgf("Cannot fetch pivotal story %d", pivotalStoryID)
 		return
 	}
 
@@ -162,7 +196,9 @@ func (s *Server) handleExpandAction(payload slack.InteractionCallback, blockActi
 
 	_, err = s.SlackClient.PostEphemeral(payload.Channel.ID, payload.User.ID, options...)
 	if err != nil {
-		fmt.Printf("%s", err)
+		log.Ctx(ctx).Warn().Err(err).Msgf("Cannot post ephemeral slack message with more info")
 		return
 	}
+
+	log.Ctx(ctx).Info().Msg("Ephemeral slack message with details is posted")
 }
--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
-	"log"
 	"os"
+	"time"
 
 	"github.com/Logiraptor/go-pivotaltracker/v5/pivotal"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/slack-go/slack"
 
 	"github.com/flexoid/slack-pivotalbot-go/internal/webservice"
 )
 
+var version = "vX.Y.Z"
+
 func main() {
+	setupLogger()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
-		log.Printf("defaulting to port %s", port)
+		log.Debug().Msgf("defaulting to port %s", port)
 	}
 
 	pivotalClient := pivotal.NewClient(os.Getenv("PIVOTAL_TOKEN"))
@@ -29,5 +35,14 @@ func main() {
 		PivotalClient:      pivotalClient,
 	}
 
+	log.Info().Msgf("Starting slack-pivotalbot %s", version)
+
 	server.Start()
+}
+
+func setupLogger() {
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+	zerolog.TimestampFunc = func() time.Time {
+		return time.Now().UTC()
+	}
 }
